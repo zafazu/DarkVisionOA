12cae00fc4997925231eb10301b495a9011d7715
zafazu <109228593+zafazu@users.noreply.github.com>
2025-12-28 16:45:44 +0100

    Delete src directory

--- src/config/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-
-contextBridge.exposeInMainWorld('Native', {
-  restart: () => ipcRenderer.send('cr'),
-  set: c => ipcRenderer.send('cs', c),
-  get: () => ipcRenderer.sendSync('cg'),
-  open: () => ipcRenderer.send('of')
-});
